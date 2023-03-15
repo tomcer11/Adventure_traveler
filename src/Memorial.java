286e74a632d2cf932682f6da5f0160fbac307fd2
Java <tomcer11@gmail.com>
2023-03-15 17:37:19 +0100

    Initial commit

---
+++ src/Memorial.java
@@ -0,0 +1,6 @@
+public class Memorial extends Place{
+
+    public void print_report(){
+        System.out.println("Pristupil si k pomniku");
+    }
+}
---
+++ src/Place.java
@@ -0,0 +1,14 @@
+public class Place {
+    private Place next_place;
+
+    public void set_next_Place(Place place) {
+        this.next_place = place;
+    }
+
+    public Place get_next_Place(){
+        return this.next_place;
+    }
+
+    public void print_report() {
+    }
+}
